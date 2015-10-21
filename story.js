0c568c23cf3b70e9e6e95905ea63637a481ccaa3
shmeadyy <lauraemead7@gmail.com>
2015-10-21 12:25:48 -0700

    additional feature updates, refactoring

--- story.js
+++ story.js
@@ -1,5 +1,7 @@
-var adjective = prompt("Please choose an adjective:");
-var noun = prompt("Please choose a noun:");
-var verb = prompt("Please choose a verb:");
+var adjective = prompt("Please type an adjective:");
+var noun = prompt("Please type a noun:");
+var verb = prompt("Please type a verb:");
 
-document.write("There once was a " + adjective + " " + noun + " who wanted to use Javascript to " + verb + " the world.")
+document.write("There once was a " + adjective + " " + noun + " who wanted to use Javascript to " + verb + " the world.");
+
+//"<h2>There once was a [adjective] programmer who wanted to use Javascript to [verb] the world."
