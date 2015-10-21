5a3cabc2e9affacbe5034712c218a4938d4c7503
shmeadyy <lauraemead7@gmail.com>
2015-10-21 12:20:18 -0700

    first commit and initial code

---
+++ story.js
@@ -0,0 +1,5 @@
+var adjective = prompt("Please choose an adjective:");
+var noun = prompt("Please choose a noun:");
+var verb = prompt("Please choose a verb:");
+
+document.write("There once was a " + adjective + " " + noun + " who wanted to use Javascript to " + verb + " the world.")
