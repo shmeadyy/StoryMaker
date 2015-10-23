af00a1ef4771f27efd160d3141d9602b88fca6f9
shmeadyy <lauraemead7@gmail.com>
2015-10-22 17:12:11 -0700

    added vowel logic

--- story.js
+++ story.js
@@ -2,6 +2,11 @@ var adjective = prompt("Please type an adjective:");
 var noun = prompt("Please type a noun:");
 var verb = prompt("Please type a verb:");
 
-document.write("There once was a " + adjective + " " + noun + " who wanted to use Javascript to " + verb + " the world.");
 
-//"<h2>There once was a [adjective] programmer who wanted to use Javascript to [verb] the world."
+if(adjective[0] == 'a' || adjective[0] == 'e' || adjective[0] == 'o' || adjective == 'u'){
+	var vowel = "an ";
+}else {
+	var vowel = "a ";
+}
+
+document.write("There once was " + vowel + adjective + " " + noun + " who wanted to use Javascript to " + verb + " the world.");
